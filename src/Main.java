b29f6e0c4e8c67c3004eef28032cb0734c5d2f69
anastasya-777 <152620467+anastasya-777@users.noreply.github.com>
2024-05-24 14:53:03 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        // Объявление переменных
-        int ticketPrice = 15590; // Стоимость билета
-        int milesForRub = 20; // Количество рублей для одной бонусной мили
-
-        // Расчёт бонусных миль
-        int bonusMiles = ticketPrice / milesForRub;
-
-        // Сохранение ответа в новую переменную
-        int totalBonusMiles = bonusMiles;
-
-        // Вывод на экран
-        System.out.println("Количество бонусных миль: " + totalBonusMiles);
+  public static void main(String[] args) {
+  
+    int ticketPrice = 15; // Укажите здесь стоимость билета
+    
+    if (ticketPrice >= 20) {
+        int bonusMiles = ticketPrice / 20;
+        System.out.println("Начислено бонусных миль: " + bonusMiles);
+    } else {
+        System.out.println("Сумма билета меньше 20 рублей, бонусные мили не начисляются.");
     }
+  }
 }
